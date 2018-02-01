5aa7aa093ded229eb19136eded6c3f18ba7f78f0
Leo Casagrande <leo@greenberry.io>
2018-02-01 12:02:34 +0100

    dernier cours

--- server.js
+++ server.js
@@ -2,22 +2,59 @@
 const express = require('express');
 var path = require('path');
 var app = express();
+var data = require("./data/data.json");
+var _ = require("lodash");
+var bodyParser = require('body-parser')
 
 // configure
 app.use('/assets', express.static('client/static'));
+app.use(bodyParser.urlencoded({ extended : false }));
+// app.use(bodyParser.json());
 
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/',check.isAdmin(req), function(req, res) {
+app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/client/index.html'));
 });
+app.get('/page', function(req, res) {
+      res.sendFile(path.join(__dirname + '/client/page.html'));
+});
 
-app.get('/test/:maquery', function(req, res) {
+app.get('/liste/:id', function(req, res) {
+      // res.sendFile(path.join(__dirname + '/client/index.html'));
+      // console.log(req);
+      var idToFind = req.params.id;
+      console.log(idToFind);
+      var index = _.findIndex(data,function(obj) {
+      	return obj.ID == idToFind;
+      });
+      console.log(index);
+      if(index >= 0){
+      	return res.json(data[index]);
+      }
+      else{
+      	return res.send(200,"object not found");
+      }
+      // res.sendStatus/(200);
+      
+});
+app.get('/liste/', function(req, res) {
       // res.sendFile(path.join(__dirname + '/client/index.html'));
       console.log(req);
+
       // res.sendStatus/(200);
-      return res.sendStatus(200);
+      return res.json(data);
 });
+
+// app.post('/test/:testparam', function(req, res) {
+//       // res.sendFile(path.join(__dirname + '/client/index.html'));
+//       console.log(req.params);
+//       console.log(req.body);
+//       // console.log(req);
+
+//       res.sendStatus(200);
+//       // return res.send(data);
+// });
 
 
 
